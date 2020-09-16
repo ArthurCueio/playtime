e0f3a9d4a751fa69de67d4a5c5a2fbc997add09d
Arthur Coelho <arthurcoelho@pm.me>
2020-09-16 18:09:38 -0300

    Style client a little bit

--- src/client/App.js
+++ src/client/App.js
@@ -9,26 +9,47 @@ export default class App extends Component {
     this.state = {
       name: '',
       result: { time: {} },
+      fetching: false,
     };
 
     this.requestPlayTime = this.requestPlayTime.bind(this);
   }
 
   requestPlayTime(accountName, region) {
+    this.setState({ fetching: true });
     fetch(`/api/getHours/${region}/${accountName}`)
       .then(res => res.json())
-      .then(time => this.setState({ result: { time }, name: accountName }));
+      .then(time => this.setState({ result: { time }, name: accountName, fetching: false }));
   }
 
   render() {
-    const { result, name } = this.state;
+    const { result, name, fetching } = this.state;
     const { time } = result;
+
+    let content;
+    if (fetching) {
+      content = (
+        <div className="loader-wrapper">
+          <div className="loader" />
+          <p>Fetching data...</p>
+        </div>
+      );
+    } else {
+      content = (
+        <div>
+          <InputForm requestFunction={this.requestPlayTime} />
+          {name !== ''
+            ? <p>{`Of the last 24 hours ${name} spent ${time.hours}:${time.minutes}:${time.seconds} playing`}</p>
+            : <p>/\ Enter summoner name and region above /\</p>}
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <InputForm requestFunction={this.requestPlayTime} />
-        { name !== ''
-          ? <p>{`Of the last 24 hours ${name} spent ${time.hours}:${time.minutes}:${time.seconds} playing`}</p>
-          : <p>/\ Enter summoner name and region above /\</p>}
+      <div className="flex-wrapper">
+        <main>
+          {content}
+        </main>
       </div>
     );
   }
