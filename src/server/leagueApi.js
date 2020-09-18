5e77409de7c49abbadc133b69cc6ba75749c6244
Arthur Coelho <arthurcoelho@pm.me>
2020-09-18 13:24:26 -0300

    Rewrite leagueApi.js with promises and catch errors

--- src/server/leagueApi.js
+++ src/server/leagueApi.js
@@ -12,32 +12,38 @@ const getBeginTime = () => moment().subtract(24, "h").valueOf();
 const getHoursInLast24 = async (accountName, region) => {
   const parsedRegion = region.toLowerCase(); // Kayn only understands lowercase region strings
 
-  const { accountId } = await kayn.Summoner.by
+  return kayn.Summoner.by
     .name(accountName)
-    .region(parsedRegion);
-  const { matches } = await kayn.Matchlist.by
-    .accountID(accountId)
     .region(parsedRegion)
-    .query({ beginTime: getBeginTime() });
+    .then(({ accountId }) => {
+      return kayn.Matchlist.by
+        .accountID(accountId)
+        .region(parsedRegion)
+        .query({ beginTime: getBeginTime() });
+    })
+    .then(({ matches }) => {
+      const gameIds = matches.map((match) => match.gameId);
+      const requests = gameIds.map((id) =>
+        kayn.Match.get(id).region(parsedRegion)
+      );
+      return Promise.all(requests).then((values) => {
+        const durations = values.map((value) => value.gameDuration);
+        let total = durations.reduce((a, b) => a + b);
 
-  const gameIds = matches.map((match) => match.gameId);
-  const requests = gameIds.map((id) => kayn.Match.get(id).region(parsedRegion));
-  return Promise.all(requests).then((values) => {
-    const durations = values.map((value) => value.gameDuration);
-    let total = durations.reduce((a, b) => a + b);
+        const result = {};
 
-    const result = {};
+        result.hours = Math.floor(total / 3600);
+        total -= result.hours * 3600;
 
-    result.hours = Math.floor(total / 3600);
-    total -= result.hours * 3600;
+        result.minutes = Math.floor(total / 60);
+        total -= result.minutes * 60;
 
-    result.minutes = Math.floor(total / 60);
-    total -= result.minutes * 60;
+        result.seconds = total;
 
-    result.seconds = total;
-
-    return result;
-  });
+        return result;
+      });
+    })
+    .catch(console.error);
 };
 
 module.exports = getHoursInLast24;
