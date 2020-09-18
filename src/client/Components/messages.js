17dfb466f1d4b2339227284deb7dc32c27eaf0c3
Arthur Coelho <arthurcoelho@pm.me>
2020-09-18 12:49:44 -0300

    Lint messages.js

--- src/client/Components/messages.js
+++ src/client/Components/messages.js
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const SucessMessage = (props) => {
-  const { name, time } = props.value;
+export const SucessMessage = ({ value }) => {
+  const { name, time } = value;
+  const { hours, minutes, seconds } = time;
 
   return (
-    <p>{`Of the last 24 hours ${name} spent ${time.hours}:${time.minutes}:${time.seconds} playing`}</p>
+    <p>{`Of the last 24 hours ${name} spent ${hours}:${minutes}:${seconds} playing`}</p>
   );
+};
+
+SucessMessage.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  value: PropTypes.object.isRequired,
 };
 
 export const ErrorMessage = () => {
